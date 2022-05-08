241b6ffd44f75d5462a8dbe0a418103b05602d1b
PLBMR <rosenberg.michael.m@gmail.com>
2022-05-07 20:52:36 -0400

    Add dagshub lgoger to the modeling module

--- src/modeling.py
+++ src/modeling.py
@@ -2,7 +2,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import roc_auc_score
 import pandas as pd
 from const import *
-
+import dagshub
 
 print(M_MOD_INIT,'\n'+M_MOD_LOAD_DATA)
 X_train = pd.read_csv(X_TRAIN_PATH)
@@ -11,7 +11,17 @@ y_train = pd.read_csv(Y_TRAIN_PATH)
 y_test = pd.read_csv(Y_TEST_PATH)
 
 print(M_MOD_RFC)
-rfc = RandomForestClassifier(n_estimators=1, random_state=0)
-rfc.fit(X_train, y_train.values.ravel())
-y_pred = rfc.predict(X_test)
-print(M_MOD_SCORE, round(roc_auc_score(y_test, y_pred),3))
+with dagshub.dagshub_logger() as logger:
+    rfc = RandomForestClassifier(n_estimators=1, random_state=0)
+    # log the model's parameters
+    logger.log_hyperparams(model_class=type(rfc).__name__)
+    logger.log_hyperparams({'model': rfc.get_params()})
+
+    # Train the model
+    rfc.fit(X_train, y_train.values.ravel())
+    y_pred = rfc.predict(X_test)
+
+    # log the model's performances
+    logger.log_metrics(
+        {f'roc_auc_score':round(roc_auc_score(y_test, y_pred), ROUND_LEV)})
+    print(M_MOD_SCORE, round(roc_auc_score(y_test, y_pred), ROUND_LEV))
